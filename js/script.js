0f10b9f217e8bd76d279f9f9e8e34439ca3df5f2
magnus <magnus.ja.gustafsson@gmail.com>
2017-05-07 16:15:18 +0200

    Can add circles with mouse

--- js/script.js
+++ js/script.js
@@ -16,6 +16,15 @@ function clickFunction(evt) {
   var x = event.clientX - $("svg").offset().left;
   var y = event.clientY - $("svg").offset().top;
   console.log("Clicked", x, y);
+  var circ = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  circ.setAttribute("cx", x.toString());
+  circ.setAttribute("cy", y.toString());
+  circ.setAttribute("r", "10");
+  circ.setAttribute("stroke", "teal");
+  circ.setAttribute("stroke-width", "2");
+  circ.setAttribute("fill", "pink");
+  $("svg").append(circ);
+  console.log("got herer!");
 }
 
 function run () {
